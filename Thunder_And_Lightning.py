f980cf1acf13a76924df00fb251f8cf247e5eba3
HunterOBrien <obrienh2@middleton.school.nz>
2023-02-12 18:45:01 +1300

    Create Thunder_And_Lightning.py

---
+++ Thunder_And_Lightning.py
@@ -0,0 +1,4 @@
+thunderLightningSeconds = float(input("What was the time between the Lightning flash and the Thunder in seconds: "))
+
+distance = thunderLightningSeconds * 340
+print(f"There was a {thunderLightningSeconds} second difference between the thunder and lightning, which means it was {distance} meters away")
